8287421ec4aaefb9cc57cb69ac2112ac845182df
Eric Wold <ericewold@gmail.com>
2020-11-28 15:24:30 -0700

    used axios get request to pull in recent posts and set up reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
+import { SET_RECENT_POSTS } from "./types";
+import axios from "axios";
+
 export function fetchRecentPosts() {
   return function (dispatch) {
-    //perform request here
-    console.log("hello");
+    axios
+      .get(`https://api.dailysmarty.com/posts`)
+      .then(response => {
+        console.log("response from get", response.data.posts);
+        dispatch({
+          type: SET_RECENT_POSTS,
+          payload: response.data.posts
+        });
+      })
+      .catch(error => {
+        console.log("error in ", error);
+      });
   };
 }
