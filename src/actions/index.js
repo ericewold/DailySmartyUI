24c6018aeb0e92eb4b18c57e42daae28b245013c
Eric Wold <ericewold@gmail.com>
2020-11-28 19:54:17 -0700

    added SET_RESULTS_POSTS reducer. now passing query results into state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SET_RECENT_POSTS } from './types';
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types';
 import axios from 'axios';
 
 export function fetchRecentPosts() {
@@ -24,10 +24,10 @@ export function fetchPostsWithQuery(query) {
       .get(`https://api.dailysmarty.com/search?q=${query}`)
       .then(response => {
         console.log('query posts', response.data.posts);
-        // dispatch({
-        //   type: SET_RECENT_POSTS,
-        //   payload: response.data.posts
-        // });
+        dispatch({
+          type: SET_RESULTS_POSTS,
+          payload: response.data.posts
+        });
       })
       .catch(error => {
         console.log('error in ', error);
